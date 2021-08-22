29b8245337b49326d1dc907a8e2bade5b5da02bf
saleh muhammad <salehmalik121@gmail.com>
2021-08-23 01:47:10 +0500

    send ss

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:send_snapshot/main.dart';
+import 'package:ss/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
